8f00b6411de7604d144b209260744302df4873ff
frossia <frossiacsb@gmail.com>
2014-01-13 17:13:40 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mossab::Application.config.secret_key_base = '1dbe8384b2f84b44be42eed2367e75da95e0559daf4e42b30be3b5666d703083b7eedb5c3f4a99530490eaaefcf9e5ee7318fa159e7bc5a331e1f976fd4cbfea'
